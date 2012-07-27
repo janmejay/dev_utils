d751e4db23ddd3131a70b46258994c05af024522
Janmejay Singh <singh.janmejay@gmail.com>
2012-07-28 01:48:51 +0530

    added a stunt in xmobar_launcher to show ellipsis in program name so most important content can be shown, but realized it generates markup which i fuck up, so will revert this next, will figure out the right thing to do with all the markup and then play this patch again

--- hackery/xmobar_launcher.c
+++ hackery/xmobar_launcher.c
@@ -2,14 +2,30 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 FILE *child_stdout = 0;
 
 const int BUFF = 2048;
+const short DOTS_LENGTH = 3;
+const short TOTAL_VISIBLE_LEN = 96;
+const short MIN_HEAD_LEN = 10;
 
 void create_child(int sig) {
     wait(-1);
     child_stdout = popen("xmobar /home/janmejay/.xmonad/xmobarrc", "w");
+}
+
+void tweek_for_visibility(char buff[]) {
+    int end = strlen(buff);
+    if (end > TOTAL_VISIBLE_LEN) {
+        short prefix_len = MIN_HEAD_LEN + DOTS_LENGTH;
+        strcpy(buff + prefix_len, buff + (end - (TOTAL_VISIBLE_LEN - prefix_len)));
+        int i;
+        for(i = 0; i < DOTS_LENGTH; i++) {
+            buff[MIN_HEAD_LEN + i] = '.';
+        }
+    }
 }
 
 int main(int argc, char** argv) {
@@ -20,6 +36,7 @@ int main(int argc, char** argv) {
 
     while(1) {
         if (fgets(line, BUFF, stdin) > 0) {
+            tweek_for_visibility(line);
             fputs(line, child_stdout);
             fflush(child_stdout);
         }
