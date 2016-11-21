b1d726ceff50e977f2a084eae32d65dd9ccde5b7
Janmejay Singh <singh.janmejay@gmail.com>
2016-11-21 19:41:16 +0530

    a bunch of things accumulated over time

--- rc/xmonad/xmonad.hs
+++ rc/xmonad/xmonad.hs
@@ -81,7 +81,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run")
+    , ((modm,               xK_p     ), spawn "dmenu_run -fn '*-5' -p '>>>' -nb '#DDD' -sb '#004' -nf '#000' -l 10")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
