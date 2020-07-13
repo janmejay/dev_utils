bede8c1084373897ba59a8c9ec0a4e4e04336937
Janmejay Singh <singh.janmejay@gmail.com>
2020-07-13 12:15:37 +0530

    some changes accumulated over time in jq lib, some changes get connman to play better with dnsmasq, docker-container start script that brings up ssh-server

---
+++ docker_start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo rc-service docker start
+
+docker_gw_ip=$(
+    ip addr \
+        | grep docker0 \
+        | grep inet \
+        | awk '{print $2}' \
+        | sed -re 's#/.+##g')
+
+function container_up {
+    sudo docker start $1
+    sudo docker exec $1 /usr/sbin/sshd
+    sudo docker exec $1 \
+         /usr/bin/sudo \
+         /bin/bash -c "echo 'nameserver $docker_gw_ip' > /etc/resolv.conf"
+}
+
+container_name=${1:-cdm_sdmain6}
+
+container_up $container_name
