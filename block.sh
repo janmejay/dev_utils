1313adce4f9802a97ca4fc35f95d58bc1c6eb5be
Janmejay Singh <singh.janmejay@gmail.com>
2020-10-09 23:04:30 +0530

    avoid attempts to populating duplicate entries picked up from different nameservers in block.sh

--- block.sh
+++ block.sh
@@ -9,7 +9,16 @@ fi
 
 sudo ipset flush blacklist
 
+def_gw=$(ip route show default | grep -Eo 'via [0-9.]+' | awk '{print $NF}')
+dns_servers="$def_gw 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220"
+
 for h in $*; do
-    dig A +short $h | xargs -n1 sudo ipset add blacklist 
-done
+    for d in $dns_servers; do
+        dig A +short @$d $h | \
+            grep -E '^[0-9.]+$'
+    done
+done | \
+    sort | \
+    uniq | \
+    xargs -n1 sudo ipset add blacklist 2>/dev/null
 
