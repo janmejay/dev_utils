5b25ef96f4761f78578120424983903f58afcf77
Janmejay Singh <janmejay.singh@sentinelone.com>
2026-06-13 23:53:19 +0530

    Reformat aerospace-window-listing script

--- aerospace_windows.sh
+++ aerospace_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sel=$(aerospace list-windows --all \
-            --format '%{window-id}│%{app-name} — %{window-title}' \
-                    | choose)
-[ -n "$sel" ] && aerospace focus --window-id "$(cut -d'│' -f1 <<<"$sel")"
+        --format '%{window-id}│%{app-name} — %{window-title}' \
+      | choose)
+[ -n "$sel" ] && aerospace focus --window-id "${sel%%│*}"
